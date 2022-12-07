db68a8aad64acde6d4991283610047fdf980139a
Nightingale <114455188+NightingaleA@users.noreply.github.com>
2022-12-07 15:31:06 +0000

    For Deleting Test commands on prod

--- Nimue/main.py
+++ Nimue/main.py
@@ -17,14 +17,16 @@ guild = discord.Object(id=725129870174322688)
 input = input_handler()
 slash = Slash_Command_Manager(tree, input)
 
+
 @client.event
 async def on_ready():
   await tree.set_translator(Discord_Translator(input.data_manager))
   tree.copy_global_to(guild=guild)
-  #tree.clear_commands(guild=guild)
-  #await  tree.sync(guild=guild)
-  await  tree.sync()
-  await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name='$help $ayuda'))
+  tree.clear_commands(guild=guild)
+  await tree.sync(guild=guild)
+  #await  tree.sync()
+  await client.change_presence(activity=discord.Activity(
+    type=discord.ActivityType.listening, name='$help $ayuda'))
 
   print(f'We have logged in as {client.user}')
 
@@ -35,7 +37,9 @@ async def on_message(message):
     return
 
   if message.content.startswith('$'):
-    response = input.get_response(message.content, message.author.display_name, message.author.display_avatar)
+    response = input.get_response(message.content, message.author.display_name,
+                                  message.author.display_avatar)
     await message.channel.send(embed=response)
+
 
 client.run(TOKEN)
