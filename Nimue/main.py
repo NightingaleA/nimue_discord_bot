65570975564fd763e4057efdfc3836f4ab123cc9
Nightingale <114455188+NightingaleA@users.noreply.github.com>
2022-12-07 15:07:20 +0000

    Initial info dump, no spanish for now

--- Nimue/main.py
+++ Nimue/main.py
@@ -21,8 +21,9 @@ slash = Slash_Command_Manager(tree, input)
 async def on_ready():
   await tree.set_translator(Discord_Translator(input.data_manager))
   tree.copy_global_to(guild=guild)
-  #tree.clear_commands(guild=None)
-  await  tree.sync(guild=guild)
+  #tree.clear_commands(guild=guild)
+  #await  tree.sync(guild=guild)
+  await  tree.sync()
   await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name='$help $ayuda'))
 
   print(f'We have logged in as {client.user}')
